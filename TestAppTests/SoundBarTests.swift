334f0a6c16a4641822451d2af388c081e4ac10d9
AtesiT <atesit@mail.ru>
2026-04-03 23:25:14 +0300

    MADE: SoundBarTests with array of funcs getItems and getMoreItems in comparison

--- TestAppTests/SoundBarTests.swift
+++ TestAppTests/SoundBarTests.swift
@@ -1,28 +1,45 @@
 import XCTest
 
 final class SoundBarTests: XCTestCase {
+    //  TODO: - Здесь создаются экземпляры SoundBar
+    
+    var items: [String] = []
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        //  Здесь происходит инициализация экземпляров SoundBar
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        //  Здесь происходит выгрузка из памяти, присваивание nil
+        
+        //  Вызываем метод super.tearDown() в конце
+        super.tearDown()
     }
-
+    
+    //  Необходимо начинать название метода с test
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        //  Тестовый тест
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
+    //  Метод для проверки производительности алгоритма
+    func testPerformanceExample() {
         measure {
-            // Put the code you want to measure the time of here.
+//             getItems()
+            getMoreItems()
         }
     }
-
+    
+    private func getItems() {
+        let numbers = 1...1000000
+        
+        for number in numbers {
+            let item = "Item: \(number)"
+            items.append(item)
+        }
+    }
+    
+    private func getMoreItems() {
+        items = (1...1000000).map {"Item: \($0)"}
+    }
 }
